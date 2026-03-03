5ebc5d8a5c3510bd39489da219f911895a0d7d21
phantekzy <mainilotfi@gmail.com>
2026-03-03 15:09:25 +0100

    created slug using nanoid and added the url and the slug to the Database

--- src/controllers/link.controller.ts
+++ src/controllers/link.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { nanoid } from "nanoid";
+import { db } from "../db";
+import { links } from "../db/schema";
 
 interface ShortenRequestBody {
   url: string;
@@ -13,6 +16,8 @@ export const shortenUrl = async (
     return res.status(400).json({ error: "URL is required!" });
   }
   try {
+    const slug = nanoid(6);
+    const [newLink] = await db.insert(links).values({ url, slug }).returning();
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal Server Error" });
