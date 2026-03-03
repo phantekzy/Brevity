d3ac062a6bee4deed0b48b19944ef4d03c63a7fc
phantekzy <mainilotfi@gmail.com>
2026-03-03 15:51:13 +0100

    finished this section

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import "dotenv/config";
 import linkRoutes from "./routes/link.routes.js";
+
 const app = express();
+
 app.use(express.json());
 app.use("/", linkRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Brevity API live on http://localhost:${PORT}`);
+});
